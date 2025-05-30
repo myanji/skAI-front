a4a03d73d44387198e47fa5f24dfb8e33fff2a27
myanji <hyean03@naver.com>
2025-05-30 14:21:31 +0900

    chore: Update environment setup and ignore rules

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    // 절대경로를 위한 alias 설정
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  // SCSS를 쓸 때마다 자동으로 index.scss 파일이 맨 위에 포함되게 하는 설정
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/shared/constants/index.scss" as *;`,
+      },
+    },
+  },
+  server: {
+    host: true,
+  },
+});
