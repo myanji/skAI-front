d35ff0e3e8caabd3187b28fe7c2961ded2ce6378
myanji <hyean03@naver.com>
2025-05-30 14:23:09 +0900

    feat: Add StepPage with interactive learning steps

---
+++ src/pages/StepPage/Step2.jsx
@@ -0,0 +1,27 @@
+import style from "./step.module.scss";
+import Header from "../../widgets/Header/Header";
+import StepCard from "../../shared/ui/StepCard/StepCard";
+import RoundedLabel from "../../shared/ui/RoundedLabel/RoundedLabel";
+import word from "./assets/word.png";
+import sentence from "./assets/sentence.png";
+import dictation from "./assets/dictation.png";
+import idioms from "./assets/idioms.png";
+
+const Step2 = () => {
+  return (
+    <div>
+      <Header />
+      <section className={style["step-container"]}>
+        <RoundedLabel text="2단계" />
+        <div className={style["card-container"]}>
+          <StepCard imageSrc={word} title={"단어 맞추기"} />
+          <StepCard imageSrc={sentence} title={"문장 순서 맞추기"} />
+          <StepCard imageSrc={dictation} title={"받아쓰기"} />
+          <StepCard imageSrc={idioms} title={"반의어 찾기"} />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Step2;
