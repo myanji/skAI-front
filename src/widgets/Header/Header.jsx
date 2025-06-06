fb9e9f4296175a2809328fe29a1bc52ec250ef0c
myanji <hyean03@naver.com>
2025-06-06 16:54:04 +0900

    feat: 프로필 수정

--- src/widgets/Header/Header.jsx
+++ src/widgets/Header/Header.jsx
@@ -1,18 +1,38 @@
 import { useState, useEffect } from "react";
 import style from "./header.module.scss";
-import profileimg from "../../pages/ProfileEditPage/assets/bear.png";
+import defaultImg from "../../pages/ProfileEditPage/assets/bear.png";
 import { useNavigate } from "react-router-dom";
+import api from "../../shared/lib/api";
 
 const Header = () => {
   const navigate = useNavigate();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [profileImage, setProfileImage] = useState(defaultImg);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
     const token = sessionStorage.getItem("token");
+
     if (token) {
       setIsLoggedIn(true);
+
+      api
+        .get("/api/profile/profile")
+        .then((res) => {
+          const userImg = res.data.image;
+          setProfileImage(userImg || defaultImg);
+        })
+        .catch((err) => {
+          console.error("프로필 이미지 불러오기 실패:", err);
+          setProfileImage(defaultImg);
+        })
+        .finally(() => {
+          setIsLoaded(true);
+        });
+    } else {
+      setIsLoaded(true);
     }
-  });
+  }, []);
 
   return (
     <header className={style["header-container"]}>
@@ -30,23 +50,23 @@ const Header = () => {
       </nav>
 
       <div>
-        {isLoggedIn ? (
+        {isLoaded && isLoggedIn ? (
           <button
             className={style["profile"]}
             onClick={() => {
               navigate("/mypage");
             }}
           >
-            <img src={profileimg} />
+            <img src={profileImage} alt="프로필" />
           </button>
-        ) : (
+        ) : isLoaded ? (
           <button
             className={style["login-btn"]}
             onClick={() => navigate("/SignInPage")}
           >
             로그인
           </button>
-        )}
+        ) : null}
       </div>
     </header>
   );
