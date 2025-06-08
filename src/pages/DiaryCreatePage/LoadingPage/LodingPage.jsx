73cbf09022a83d21acad6b3e67cad7d1177ac958
myanji <hyean03@naver.com>
2025-06-09 03:18:02 +0900

    style: 로딩화면 애니메이션 추가

--- src/pages/DiaryCreatePage/LoadingPage/LodingPage.jsx
+++ src/pages/DiaryCreatePage/LoadingPage/LodingPage.jsx
@@ -1,12 +1,24 @@
+/* src/pages/LoadingPage/LoadingPage.jsx */
 import Header from "../../../widgets/Header/Header";
 import Loading from "../assets/Loading.png";
 import style from "./loading-page.module.scss";
 
 const LoadingPage = () => {
+  const text = "그림을 만들고 있어요...".split("");
   return (
-    <div className={style["layout"]}>
+    <div className={style.layout}>
       <div className={style["loading-container"]}>
-        <h3>그림을 만들고 있어요...</h3>
+        <h3 className={style.animatedText}>
+          {text.map((ch, i) => (
+            <span
+              key={i}
+              style={{ animationDelay: `${i * 0.1}s` }}
+              dangerouslySetInnerHTML={{
+                __html: ch === " " ? "&nbsp;" : ch,
+              }}
+            />
+          ))}
+        </h3>
         <p>만드는데 3분정도 소요되니 조금만 기다려주세요!</p>
         <img src={Loading} alt="로딩중..." />
       </div>
