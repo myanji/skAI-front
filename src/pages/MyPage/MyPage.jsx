0baff918c56b3058baca0773939b943b8aa7da4e
myanji <hyean03@naver.com>
2025-06-11 00:20:12 +0900

    build: netlify 경로 요청 404 문제 수정

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../../shared/lib/api";
 import ProfileCard from "../../features/profile/ProfileCard/ProfileCard";
-import Header from "../../widgets/Header/Header";
 import profileImg from "../ProfileEditPage/assets/bear.png";
 import style from "./mypage.module.scss";
 import ProfileBtn from "../../shared/ui/Button/ProfileBtn/ProfileBtn";
